d8c5218ed1548fdd6eb67f687b3e9d0c575a36bf
Javi Palacios <javi@fjp.es>
2018-07-20 21:05:32 +0200

    Adding stylesheets for the header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  # Return 'active' string if the passed path is exactly the same as the
+  # current path.
+  def active?(path)
+    'active' if request.path == path
+  end
+
+  # Return 'active' string if the passed locale is the same as the current locale.
+  def current_locale?(locale)
+    'active' if I18n.locale == locale
+  end
 end
