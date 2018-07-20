caa9e111759dbb10931efd1ea59a9205643f2502
Javi Palacios <javi@fjp.es>
2018-07-20 21:15:59 +0200

    Adding url field for User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_18_143901) do
+ActiveRecord::Schema.define(version: 2018_07_20_191120) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2018_07_18_143901) do
     t.string "facebook"
     t.integer "job"
     t.string "name"
+    t.string "url"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
