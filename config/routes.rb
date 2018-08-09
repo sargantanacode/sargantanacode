dae254d1bdf0a4f8b389edcf31778a5b888dad25
Javi Palacios <javi@fjp.es>
2018-08-09 11:05:12 +0200

    Adding redirected routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :show]
     root to: "posts#index", as: "homepage"
 
+    # Simplifying public-zone's menu links
+    get "/about-us/" => "posts#show", defaults: {id: "about-us"}, as: "about_us"
+
+    # Redirecting old links for the new ones
+    get "/category/:category" => redirect("/categories/%{category}")
+
     namespace :admin do
       resources :posts, except: [:show] do
         put :publish, as: "publish"
@@ -15,7 +21,7 @@ Rails.application.routes.draw do
       resources :users, except: [:new, :create]
       resources :categories, except: [:show]
     end
-  end  
+  end
   get "/*path" => "application#change_path", constraints: { path: /(?!(#{I18n.available_locales.join("|")})\/).*/ }
   root to: "application#change_path"
 end
