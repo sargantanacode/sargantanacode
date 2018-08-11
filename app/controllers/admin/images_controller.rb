c87ce6070a36ff53efd30ede9ff9c062303e99d0
Javi Palacios <javi@fjp.es>
2018-08-11 12:16:14 +0200

    Adding new Images

--- app/controllers/admin/images_controller.rb
+++ app/controllers/admin/images_controller.rb
@@ -7,17 +7,31 @@ class Admin::ImagesController < ApplicationController
   end
 
   def new
+    @image = Image.new
   end
 
   def edit
   end
 
   def create
+    @image = Image.new(image_params)
+    if @image.save
+      redirect_to admin_images_path, notice: t('.created')
+    else
+      render :new
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def image_params
+    permitted = Image.globalize_attribute_names + [:image]
+    params.require(:image).permit(permitted)
   end
 end
