93d3b1338f980cb9bf059958375a3dc8c0b9a3f5
Javi Palacios <javi@fjp.es>
2018-08-15 23:54:18 +0200

    Finishing the Comment Controller

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,6 +1,7 @@
 class Admin::CommentsController < ApplicationController
   include ApplicationHelper
   before_action :only_admins
+  before_action :set_comment, only: [:edit, :update, :destroy]
   before_action :set_comment_id, only: [:approve]
 
   def index
@@ -11,20 +12,38 @@ class Admin::CommentsController < ApplicationController
   end
 
   def update
+    if @comment.update(comment_params)
+      flash[:notice] = t('.saved')
+      redirect_back(fallback_location: admin_dashboard_path)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @comment.destroy
+    flash[:notice] = t('.destroyed')
+    redirect_back(fallback_location: admin_dashboard_path)
   end
 
   def approve
     @comment.approve
     flash[:notice] = t('.approved')
-    redirect_back(fallback_location: homepage_path)
+    redirect_back(fallback_location: admin_dashboard_path)
   end
 
   private
 
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
   def set_comment_id
     @comment = Comment.find(params[:comment_id])
+  end
+
+  def comment_params
+    permitted = [:author, :email, :url, :comment, :status]
+    params.require(:comment).permit(permitted)
   end
 end
