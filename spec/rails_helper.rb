f5aa1b60d5774079aa7f071ff36fb9bbf768defd
Javi Palacios <javi@fjp.es>
2018-08-19 17:51:13 +0200

    Testing Travis-CI configurations Vol. V --WTF

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -56,4 +56,10 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.before(:each, type: :system) do
+    driven_by :selenium, using: :chrome, options: {
+      args: ["headless", "disable-gpu", "no-sandbox", "disable-dev-shm-usage"]
+    }
+  end
 end
