963fd2ff5e14102c4100740aa997c105dccb39e4
Javi Palacios <javi@fjp.es>
2018-08-19 14:10:35 +0200

    Updating dependencies (20180819)

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,13 +2,13 @@ require 'faker'
 
 FactoryBot.define do
   factory :user do
-    name Faker::LordOfTheRings.unique.character
-    email Faker::Internet.unique.email
-    password Faker::Internet.password
-    role :user
+    name { Faker::Name.unique.name }
+    email { Faker::Internet.unique.email }
+    password  {Faker::Internet.password }
+    role { :user }
   end
 
   trait :admin do
-    role :admin
+    role { :admin }
   end
 end
